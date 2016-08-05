75c92fe2fe2731861585a5a86a7960a6e625f2f0
John Axel Eriksson <john@insane.se>
2016-08-05 10:23:26 +0200

    Add pg_functions (only some added for now).

---
+++ lib/relativity/pg_functions.lua
@@ -0,0 +1,32 @@
+local c, of, f, sql
+do
+  local _obj_0 = require('relativity')
+  c, of, f, sql = _obj_0.cast, _obj_0.opt_func, _obj_0.func, _obj_0.sql
+end
+local abs = math.abs
+local localtimestamp = f('localtimestamp')
+local LOCALTIMESTAMP
+LOCALTIMESTAMP = function()
+  return localtimestamp(6)
+end
+local interval
+interval = function(spec)
+  return c(spec, 'interval')
+end
+return {
+  ts_rank = f('ts_rank'),
+  to_tsquery = f('to_tsquery'),
+  any = f('ANY'),
+  to_json = f('to_json'),
+  json_build_object = of('json_build_object'),
+  array_agg = f('array_agg'),
+  concat = f('concat'),
+  localtimestamp = localtimestamp,
+  LOCALTIMESTAMP = LOCALTIMESTAMP,
+  interval = interval,
+  from_now = function(num, spec)
+    local sign = num < 0 and '-' or '+'
+    local abs_num = abs(num)
+    return sql(LOCALTIMESTAMP():to_sql() .. " " .. tostring(sign) .. " " .. interval(tostring(abs_num) .. " " .. tostring(spec)):to_sql())
+  end
+}
---
+++ lib/relativity/pg_functions.moon
@@ -0,0 +1,24 @@
+{cast: c, opt_func: of, func: f, :sql} = require'relativity'
+
+abs = math.abs
+
+localtimestamp = f'localtimestamp'
+LOCALTIMESTAMP = -> localtimestamp 6
+interval = (spec) -> c spec, 'interval'
+
+{
+  ts_rank: f'ts_rank'
+  to_tsquery: f'to_tsquery'
+  any: f'ANY'
+  to_json: f'to_json'
+  json_build_object: of'json_build_object'
+  array_agg: f'array_agg'
+  concat: f'concat'
+  :localtimestamp
+  :LOCALTIMESTAMP
+  :interval
+  from_now: (num, spec) ->
+    sign = num < 0 and '-' or '+'
+    abs_num = abs num
+    sql LOCALTIMESTAMP!\to_sql! .. " #{sign} " .. interval"#{abs_num} #{spec}"\to_sql!
+}
